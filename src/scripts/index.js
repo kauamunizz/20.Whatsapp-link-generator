ba5801ccfaa2c61c440239cdb55316e85e7230b3
Kauã Muniz <kauamunizz@hotmail.com>
2022-09-02 23:37:37 -0300

    js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,36 @@
-import '../styles/style.scss'
+import '../styles/style.scss';
+
+"strict mode"
+
+const index = (() => {
+    function phoneNumber() {
+        const { phone, text } = document.forms.formPhone;
+        const valor = `https://api.whatsapp.com/send?phone=${phone.value}&text=${text.value}`;
+        const copy = document.querySelector('#copy');
+
+        copy.innerText = valor;
+    }
+
+    function events() {
+        document.forms.formPhone.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            phoneNumber();
+
+            document.querySelector('#hide').classList.remove('hide');
+        })
+        
+    }
+
+
+    function init() {
+        events();
+    }
+
+
+    return {
+        init
+    }
+})();
+
+document.addEventListener('DOMContentLoaded', index.init);
